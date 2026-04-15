4defdf5238d40c3bfeca81363dd08bf431f62814
Leon <leonmuigaike@gmail.com>
2026-04-15 12:01:25 +0300

    Completed Flatburger scope lab

---
+++ index.js
@@ -0,0 +1,21 @@
+let burgers = ["Hamburger", "Cheeseburger"];
+let drink = "milkshake";
+let featuredItem = "";
+
+function setFeaturedItem(item) {
+    featuredItem = item;
+}
+
+function addSpecialBurger() {
+    if (true) {
+        let specialBurger = "BBQ Bacon Burger";
+        burgers.push(specialBurger);
+    }
+}
+
+setFeaturedItem("Double Cheeseburger");
+addSpecialBurger();
+
+console.log(burgers);
+console.log(drink);
+console.log(featuredItem);
